95adcb63df85773641aab33d8402a033c9413de1
Brennan <bjwadswo@gmail.com>
2025-03-21 11:52:27 +0100

    Training on FVB
    New model trained on FVB video. Validated to work well with greater than 70% confidence in identifying both C57Bl/6 and FVB mice.

--- Mouse_Treadmill_Computer_Vision/video_to_frame.py
+++ Mouse_Treadmill_Computer_Vision/video_to_frame.py
@@ -16,17 +16,18 @@ def main(load,save):
     for root, dirs, files in os.walk(load):
         video_list = files # get list of all videos in the video directory
     for i, video in enumerate(video_list):
-        vidObj = cv2.VideoCapture(os.path.join(load,video)) # collects video object in cv2 framework
-        count = 0 # count the frame number
-        success = 1 # track when reached the end of the video
-        while success:
-            success, img = vidObj.read() # this will step by step read each frame of the video
-            # don't want to save every frame. There was ~30k frames in the smallest 15min video
-            # Guess that there is a total of 200-250k frames in the five selected training videos
-            # want ~250-500 training images, try saving every 1000th frame
-            if count % 1000 == 0: # execute code every 1000th frame
-                cv2.imwrite(f"{save}vid{i}frame{count}.jpg", img) # save within the frames folder, record video number and frame number
-            count += 1 # count next frame
+        if str(video)[0] != ".":
+            vidObj = cv2.VideoCapture(os.path.join(load,video)) # collects video object in cv2 framework
+            count = 0 # count the frame number
+            success = 1 # track when reached the end of the video
+            while success:
+                success, img = vidObj.read() # this will step by step read each frame of the video
+                # don't want to save every frame. There was ~30k frames in the smallest 15min video
+                # Guess that there is a total of 200-250k frames in the five selected training videos
+                # want ~250-500 training images, try saving every 1000th frame
+                if count % 1000 == 0: # execute code every 1000th frame
+                    cv2.imwrite(f"{save}vid{i}frame{count}.jpg", img) # save within the frames folder, record video number and frame number
+                count += 1 # count next frame
 
 
 if __name__ == '__main__':
